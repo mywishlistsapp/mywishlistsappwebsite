5bd2649658b14eda6b71e53319cc9ee623e1f55f
Matthew Orres <matthew.orres@gmail.com>
2012-06-25 13:48:59 -0400

    Upload of site.

---
+++ js/wishlist.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+	var animationTimespan = 500;
+	
+	setupBindings(animationTimespan);
+	
+	setupMasks();
+	
+	setupValidation();
+});
+
+function setupMasks() {
+	$('input.zip').mask('99999');
+	$('input.phone').mask('(999) 999 - 9999');
+}
+
+function setupBindings(animationTimespan) {
+	$('span.home').on('click', function() {
+		var $link = $(this);
+		
+		$('html, body').animate({
+			scrollTop: $('div.homepage').offset().top
+			}, animationTimespan, function() { 
+					changeToolbarColor('home'); 
+					changeActiveLink($link);
+				});
+	});
+	
+	$('span.features').on('click', function() {
+		var $link = $(this);
+		
+		$('html, body').animate({
+			scrollTop: $('div.featurespage').offset().top
+			}, animationTimespan, function() { 
+					changeToolbarColor('features'); 
+					changeActiveLink($link);
+				});
+	});
+	
+	$('span.contact').on('click', function() {
+		var $link = $(this);
+		
+		$('html, body').animate({
+			scrollTop: $('div.contactpage').offset().top
+			}, animationTimespan, function() { 
+					changeToolbarColor('contact'); 
+					changeActiveLink($link);
+				});
+	});
+	
+	$(document).on('scroll', function(evt) {
+		var $header = $('header'),
+			yPos = $header.offset().top,
+			topOfFeatures = 1120,
+			botOfFeatures = 2250,
+			isHomePage = yPos <= topOfFeatures,
+			isFeaturesPage = yPos >= topOfFeatures && yPos < botOfFeatures,
+			isContactPage = yPos >= botOfFeatures;
+		
+		if(isHomePage) {
+			changeActiveLink($('span.home'));
+			$header.removeClass('light');
+		} else if(isFeaturesPage) {
+			changeActiveLink($('span.features'));
+			$header.addClass('light');
+		} else if(isContactPage) {
+			changeActiveLink($('span.contact'));
+			$header.removeClass('light');
+		}
+	});
+	
+	$('div.contactpage button').on('click', function(ev) {
+		ev.preventDefault();
+		
+		contactSubmitHandler();
+		
+		return false;
+	});
+}
+
+function changeToolbarColor(section) {
+	if(section == "features") {
+		$('header').addClass('light');
+	} else {
+		$('header').removeClass('light');
+	}
+}
+
+function changeActiveLink($link) {
+	$('header span.active').removeClass('active');
+	$link.addClass('active');
+}
+
+function setupValidation() {
+	$('div.contactpage form').validate({
+		rules: {
+			first: "required",
+			last: "required",
+			address: "required",
+			city: "required",
+			state: "required",
+			zip: "required",
+			phone: "required",
+			email: "required email",
+			message: "required"
+		},
+		errorClass: 'invalid',
+		errorPlacement: function(error, element) {
+			//no-op
+		}
+	});
+}
+
+function contactSubmitHandler() {
+	var $form = $('div.contactpage form');
+	
+	if($form.valid()) {
+		alert('Yay valid');
+	} else {
+		return false;
+	}
+}
